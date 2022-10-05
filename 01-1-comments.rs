67df6b557cf39e8bdc7d6f73e6ad6f2ad9a46c43
Joe Hare <joseph.hare@gmail.com>
2022-10-04 20:41:19 -0500

    u2d

---
+++ 01-1-comments.rs
@@ -0,0 +1,22 @@
+fn main() {
+    // add line comment
+    println!("hear me now"); // this println! is a "macro"
+
+    /*
+     * block shit here
+     *
+     */
+    println!("made it");
+
+    // okay this is fixed
+    //
+
+    /// this is make a doc for following
+    ///
+
+    //! is make a doc for the enclosed...
+    //!
+
+
+         
+}
---
+++ 01-2-formatted-print.rs
@@ -0,0 +1,6 @@
+fn main() {
+    // not delving to 1.2.1-3 for now
+
+
+    println!("{} these get replaced", 42);
+}
